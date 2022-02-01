fbd10f2096cd8e056ce8eaa1d6ceb0a8c745ef9f
gustavobr44 <43259296+gustavobr44@users.noreply.github.com>
2022-02-01 03:36:45 -0300

    Adicionado página de detalhes da playlist

--- public/js/home.js
+++ public/js/home.js
@@ -29,7 +29,6 @@ function getPlaylistsSpotify(appid, imageurl, tracksqnt, owner, name, collaborat
     + "<div class=playlist_item>" + owner +"</div>"
     + "<div class=playlist_item>Collab: "+ collaborative +"</div>"
     
-    console.log(typeof appid)
     block.onclick = () => {
         showPlaylistDetails(appid, imageurl, tracksqnt, owner, name, collaborative);
     };
@@ -38,7 +37,32 @@ function getPlaylistsSpotify(appid, imageurl, tracksqnt, owner, name, collaborat
 }
 
 function showPlaylistDetails(appid, imageurl, tracksqnt, owner, name, collaborative) {
-    var block = document.createElement("");
-    block.innerHTML = ""
-    + "<div class=playlist-details></div>";
+    var block = document.createElement("div");
+    block.classList.add("playlist-details-background");
+    block.id = "playlist-details";
+    
+    block.innerHTML = "<div class=\"playlist-details-container\">"
+    + "<div class=\"playlist-details-top\">"
+    + "<img src=\"" + imageurl + "\" alt=\"\" height=\"100%\" class=\"playlist-details-image\"/>"
+    + "<div>"
+    + "<h2>" + name + "</h2>"
+    + "<h4>Nº de músicas: " + tracksqnt + "</h4>"
+    + "<h4>Dono: " + owner + "</h4>"
+    + ((collaborative === false) ? "<h4>Colaborativa? Não</h4>" : "<h4>Colaborativa? Sim</h4>")
+    + "</div>"
+    + "<div class=\"playlist-details-trash\">"
+    + "<button class=\"playlist-details-trash-button\"><img src=\"/assets/images/lixo.png\" alt=\"\" width=\"100%\"/></button>"
+    + "</div>"
+    + "</div>"
+    + "<div class=\"playlist-details-bottom\">"
+    + "<a href=\"https://open.spotify.com/playlist/" + appid + "\" target=\"_blank\" rel=\"noopener noreferrer\" class=\"playlist-details-goto-link\"><button class=\"playlist-details-goto-button\"><h3>Ir para a playlist!</h3></button></a>"
+    + "</div>"
+    + "<button class=\"playlist-details-close-button\" onclick=\"hidePlaylistDetails()\">X</button>"
+    + "</div>";
+
+    document.getElementById("playlist-details-parent").insertBefore(block, document.getElementById("playlist-details-parent").firstChild);
+}
+
+function hidePlaylistDetails() {
+    document.getElementById("playlist-details").remove();
 }
